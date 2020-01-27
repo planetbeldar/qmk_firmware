edcfdaaea45c8cb5c05ac5403c13fc7c490ef6c8
Tony Höglund <tony.hoglund@gmail.com>
2020-01-27 16:29:30 +0100

    refactoring ut47.2 keymap, adding tri layer

--- keyboards/ut472/keymaps/planetbeldar/keymap.c
+++ keyboards/ut472/keymaps/planetbeldar/keymap.c
@@ -15,11 +15,28 @@
  */
 #include QMK_KEYBOARD_H
 
-#define LT3_TAB LT(3, KC_TAB)
+enum my_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _TRI,
+  _ADJUST,
+  _NAVIGATION
+};
+
+enum my_keycodes {
+  LOWER = SAFE_RANGE,
+  RAISE,
+  ADJUST,
+  NAVIGATION
+};
+
+#define LT_ADJUST_TAB LT(_ADJUST, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Base Layer
+   * QWERTY
    * ,-------------------------------------------------------------------------.
    * | Tab |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |Bspace |
    * |-------------------------------------------------------------------------+
@@ -27,39 +44,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------------------+
    * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Sh/En|
    * |-------------------------------------------------------------------------+
-   * | Ctrl| Ctrl| Alt | Gui |  L2  |   Space   |  L1  | Left| Down|  Up |Right|
+   * | NAV | Ctrl| Alt | Gui |LOWER |   Space   | RAISE|AltGr| Down|  Up |Right|
    * `-------------------------------------------------------------------------'
-   */
-
-LAYOUT(
-  LT3_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  */
+[_QWERTY] = LAYOUT(
+  LT_ADJUST_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  MO(4),          KC_LCTL, KC_LALT, KC_LGUI, MO(2),      KC_SPC,        MO(1),   KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
+  NAVIGATION,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-  /* FN Layer 1
+  /* Lower Layer 
    * ,-------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
-   * |-------------------------------------------------------------------------+
-   * |      |     |     |     |     |     |     |  -  |  =  |  [  |  ]  |  \   |
-   * |-------------------------------------------------------------------------+
-   * |       | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |     |
-   * |-------------------------------------------------------------------------+
-   * |     |     |     |Capsl|      |          |       | Home| PgDn| PgUp| End |
-   * `-------------------------------------------------------------------------'
-   */
-
-LAYOUT( /* Right */
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
-  _______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
-),
-
-  /* FN Layer 2
-   * ,-------------------------------------------------------------------------.
-   * |   ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |Delete|
+   * |   ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |Bspace|
    * |-------------------------------------------------------------------------+
    * |      |     |     |     |     |     |     |  _  |  +  |  { |  }  |   |   |
    * |-------------------------------------------------------------------------+
@@ -68,15 +65,50 @@ LAYOUT( /* Right */
    * |     |     |     |Capsl|      |          |       | Home| PgDn| PgUp| End |
    * `-------------------------------------------------------------------------'
    */
-
-LAYOUT( /* Left */
-  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
+[_LOWER] = LAYOUT(
+  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_BSPC,
   _______, _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
-  /* FN Layer 3
+  /* Raise Layer
+   * ,-------------------------------------------------------------------------.
+   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |Bspace |
+   * |-------------------------------------------------------------------------+
+   * |      |     |     |     |     |     |     |  -  |  =  |  [  |  ]  |  \   |
+   * |-------------------------------------------------------------------------+
+   * |       | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |     |
+   * |-------------------------------------------------------------------------+
+   * |     |     |     |Capsl|      |          |       | Home| PgDn| PgUp| End |
+   * `-------------------------------------------------------------------------'
+   */
+[_RAISE] = LAYOUT(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  _______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
+  _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+),
+
+  /* Tri Layer
+   * ,-------------------------------------------------------------------------.
+   * |     |     |     |     |     |     |     |     |     |     |     | Delete|
+   * |-------------------------------------------------------------------------+
+   * |      |     |     |     |     |     |     |     |     |     |     |      |
+   * |-------------------------------------------------------------------------+
+   * |       |     |     |     |     |     |     |     |     |     |     |     |
+   * |-------------------------------------------------------------------------+
+   * |     |     |     |     |      |          |       |     |     |     |     |
+   * `-------------------------------------------------------------------------'
+   */
+[_TRI] = LAYOUT(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+),
+
+  /* Adjust Layer
    * Backlighting, keyboard controls, music etc.
    * ,-------------------------------------------------------------------------.
    * | Esc | Calc|Webhm| Mail| Comp|     |     |     |     |     |PrtSc|  Vol+ |
@@ -88,19 +120,60 @@ LAYOUT( /* Left */
    * |Sleep|     |     |     |      |   C+A+D  | C+A+I |     | Prev|Pause| Next|
    * `-------------------------------------------------------------------------'
   */
-
-LAYOUT(
+[_ADJUST] = LAYOUT(
   KC_ESC,  _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU,
   _______, VLK_TOG, RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, KC_VOLD,
   _______, RGB_TOG, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, KC_MSTP, KC_MUTE,
   KC_SLEP, _______, _______,  _______, _______,  LCA(KC_DEL), LCA(KC_INS), _______, KC_MPRV, KC_MPLY, KC_MNXT
 ),
 
-
-LAYOUT( /* Navigation */
+  /* Navigation Layer
+   * ,-------------------------------------------------------------------------.
+   * |     |     |     |     |     |     |     |     |     |     |     |       |
+   * |-------------------------------------------------------------------------+
+   * |      |     |     |     |     |     |     | Left| Down|  Up |Right|      |
+   * |-------------------------------------------------------------------------+
+   * |       |     |     |     |     |     |     |     |     |     |     |     |
+   * |-------------------------------------------------------------------------+
+   * |     |     |     |     |      |          |       |MLeft|MDown| MUp |MRigh|
+   * `-------------------------------------------------------------------------'
+   */
+[_NAVIGATION] = LAYOUT( /* Navigation */
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
   _______, _______, _______, _______, _______,     _______,      _______, KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R
-),
+)
 };
+
+void process_layer_change(enum my_layers layer, keyrecord_t *record) {
+  if (record->event.pressed) {
+    layer_on(layer);
+  }
+  else {
+    layer_off(layer);
+  }
+
+  if (layer == _LOWER || layer == _RAISE) {
+    update_tri_layer(_LOWER, _RAISE, _TRI);
+  }
+}
+ 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
+    case LOWER:
+      process_layer_change(_LOWER, record);
+      return false;
+    case RAISE:
+      process_layer_change(_RAISE, record);
+      return false;
+    case ADJUST:
+      process_layer_change(_ADJUST, record);
+      return false;
+    case NAVIGATION:
+      process_layer_change(_NAVIGATION, record);
+      return false;
+    default:
+      return true; 
+  }
+}
