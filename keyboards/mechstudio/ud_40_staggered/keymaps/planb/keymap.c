8d4e9bef3a688da29392bb9961d142f5e558fe86
Tony Höglund <tony.hoglund@gmail.com>
2021-02-18 11:04:46 +0100

    ud_40 tweaking and tri layer

--- keyboards/mechstudio/ud_40_staggered/keymaps/planb/keymap.c
+++ keyboards/mechstudio/ud_40_staggered/keymaps/planb/keymap.c
@@ -2,7 +2,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
-enum layer_names {
+enum layers {
     _BASE,
     _FN1,
     _FN2,
@@ -39,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
  * │LShift│ Z │ X │ C │ V │ B │ N │ M │ , │ . │Sft/│
  * ├────┬─┴─┬─┴──┬┴───┴──┬┴───┴───┴┬──┴──┬┴───┼────┤
- * │Navi│Alt│ OS │LN2Spac│ LN1Space│RAlt │    │Navi│
+ * │Navi│Alt│ OS │FN2Spac│ FN1Space│RAlt │    │Navi│
  * └────┴───┴────┴───────┴─────────┴─────┴────┴────┘
 */
 [_BASE] = LAYOUT_staggered(
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LCT_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,         KC_SCLN,
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSF_SLS,
-  MO(_NAV), KC_LALT, KC_LGUI,   LT_FN2_SPACE,   _______, LT_FN1_SPACE,     KC_RALT,     _______,      MO(_NAV)
+  MO(_NAV), KC_LALT, KC_LGUI,   LT_FN2_SPACE,   LT_FN1_SPACE, LT_FN1_SPACE,     KC_RALT,     _______, MO(_NAV)
 ),
 
 /* FN1 Layer
@@ -69,27 +69,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* FN2 Layer
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
- * │ ~ │ ~ │ @ │ # │ $ │ % │ ^ │ & │ * │ ( │ ) │ \ │
+ * │ ~ │ ! │ @ │ # │ $ │ % │ ^ │ & │ * │ ( │ ) │ \ │
  * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
  * │    │   │   │   │   │   │   │ _ │ + │ { │   }  │
  * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
  * │      │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10 │
  * ├────┬─┴─┬─┴──┬┴───┴──┬┴───┴───┴┬──┴──┬┴───┼────┤
- * │    │   │    │       │         │     │    │    │
+ * │    │   │    │       │         │     │ RES│    │
  * └────┴───┴────┴───────┴─────────┴─────┴────┴────┘
  */
 [_FN2] = LAYOUT_staggered(
   KC_TILDE,KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
   _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR,      KC_RCBR,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  _______, _______, _______,   _______,        _______, _______,          _______,      _______,     _______
+  _______, _______, _______,   _______,        _______, _______,          _______,      RESET,      _______
 ),
 
 /* NAV Layer
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
  * │   │   │   │   │   │   │   │   │   │   │   │   │
  * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │    │   │   │   │   │   │   │   │   │   │      │
+ * │    │   │   │   │   │   │ ← │ ↓ │ ↑ │ → │ Enter│
  * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
  * │      │   │   │   │   │   │   │   │   │   │    │
  * ├────┬─┴─┬─┴──┬┴───┴──┬┴───┴───┴┬──┴──┬┴───┼────┤
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
  * │RES│   │   │   │   │   │   │   │   │   │   │DEL│
  * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │    │   │   │   │   │   │   │   │   │   │      │
+ * │    │   │   │   │   │   │   │   │   │   │   |  │
  * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
  * │      │   │   │   │   │   │   │   │   │   │    │
  * ├────┬─┴─┬─┴──┬┴───┴──┬┴───┴───┴┬──┴──┬┴───┼────┤
@@ -116,14 +116,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_TRI] = LAYOUT_staggered(
   RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      KC_PIPE,
   _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
   _______, _______, _______,   _______,        _______, _______,          _______,      _______,     _______
 ),
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _FN1, _FN2, _TRI);
+}
 
-/* TEMPLATE
+
+/* TEMPLATE STAGGERED SPLIT SPACE
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
  * │   │   │   │   │   │   │   │   │   │   │   │   │
  * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
